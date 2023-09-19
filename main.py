96fbc8969a73179eb195eea18ab48511d3d23be3
Gergő Mézeš <gergomezesvg@gmail.com>
2023-09-19 20:03:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ import subprocess
 # Zeptat se uživatele na URL
 target_url = input("Zadejte cílovou URL: ").strip()
 
-# Funkce pro spuštění nástroje WPScan
-def run_wpscan(target):
-    result = subprocess.run(["wpscan", "--url", target], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    return result.stdout, result.stderr
+# Název souboru pro uložení výstupu
+output_file = "wpscan_output.txt"
 
-# Výstupy z WPScan
-wpscan_output, wpscan_error = run_wpscan(target_url)
+# Funkce pro spuštění nástroje WPScan s sudo a zápisem do souboru
+def run_wpscan(target):
+    with open(output_file, "w") as output:
+        result = subprocess.run(["sudo", "wpscan", "--url", target], stdout=output, stderr=subprocess.PIPE, text=True)
+    return result.stderr
+
+# Spustit WPScan a uložit výstup do souboru
+wpscan_error = run_wpscan(target_url)
 
 # Výstup odeslán zpět na webové rozhraní
 print("Content-Type: text/html")
@@ -19,8 +23,6 @@ print("<html>")
 print("<head><title>WPScan Results</title></head>")
 print("<body>")
 print("<h1>WPScan Results</h1>")
-print("<pre>")
-print(wpscan_output)
-print("</pre>")
+print("<p>Výstup WPScan byl uložen do souboru: {}</p>".format(output_file))
 print("</body>")
 print("</html>")
